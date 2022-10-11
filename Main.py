2bad6f651204071995f99e5b25878f8a85f6102d
Melvin2002-cs <110010657+Melvin2002-cs@users.noreply.github.com>
2022-10-11 11:58:13 +0530

    1st try

--- Main.py
+++ Main.py
@@ -4,19 +4,35 @@ class BinaryTreeNode:
         self.left_child = None
         self.right_child = None
 
+        
+        
+
 
 def insert(root, new_value) -> BinaryTreeNode:
-    """If binary search tree is empty, make a new node, declare it as root and return the root.
-        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
-        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
-        Finally, return the root.
-        """
+    
     # Write your code here
+    if root==None:
+        root=BinaryTreeNode(new_value)
+    else:
+        temp=root
+        while(temp!=None):
+            if temp.data>new_value:
+                temp=temp.left_child
+            else:
+                temp=temp.right_child
+        temp=BinaryTreeNode(new_value)
 
 
 def inorder(root) -> None:
     # Write your code here
-
+    if root.left_child==None:
+        print(root.data,end=" ")
+        return
+    inorder(root.left_child)
+    print(root.data,end=" ")
+    inorder(root.right_child)
+    
+    
 
 def preorder(root) -> None:
     # Write your code here
@@ -41,3 +57,12 @@ print()
 preorder(root)
 print()
 postorder(root)
+
+
+
+
+"""If binary search tree is empty, make a new node, declare it as root and return the root.
+        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
+        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
+        Finally, return the root.
+        """
